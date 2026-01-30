b7ed76df4edfee78ca6d45f48ea2293497d9d370
STARK <misssqn@gmail.com>
2026-01-30 20:17:58 +0530

    Create config.py

---
+++ config.py
@@ -0,0 +1,13 @@
+import os
+
+# Telegram API Details (Get from my.telegram.org)
+API_ID = int(os.getenv("API_ID", "1234567")) 
+API_HASH = os.getenv("API_HASH", "your_api_hash_here")
+
+# Bot Token (Get from @BotFather)
+BOT_TOKEN = os.getenv("BOT_TOKEN", "your_bot_token_here")
+
+# Support Links
+SUPPORT_CHAT = "https://t.me/YourSupportGroup"
+SUPPORT_CHANNEL = "https://t.me/YourChannel"
+OWNER_USERNAME = "YourUsername"
