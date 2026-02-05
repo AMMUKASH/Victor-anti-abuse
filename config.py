6e9525f13b2e9df535b8463afef741416dcedbca
STARK <misssqn@gmail.com>
2026-02-05 18:03:21 +0530

    Update config.py

--- config.py
+++ config.py
@@ -5,9 +5,13 @@ API_ID = int(os.getenv("API_ID", "34135757"))
 API_HASH = os.getenv("API_HASH", "d3d5548fe0d98eb1fb793c2c37c9e5c8")
 BOT_TOKEN = os.getenv("BOT_TOKEN", "8508791428:AAEVP8eN8H4yE1AVwSs15KpNV3TamY4hXB8")
 
+# --- Database Setup ---
+# Password 'VICTOR01' ke sath Mongo URL
+MONGO_URL = os.getenv("MONGO_URL", "mongodb+srv://Victor:VICTOR01@cluster0.mongodb.net/?retryWrites=true&w=majority")
+
 # --- Community & Identity ---
-OWNER_ID = 8482447535
-LOG_GROUP_ID = -1003867805165  # Yahan apni Group ID daalein
-OWNER_LINK = "https://t.me/XenoEmpir"
-SUPPORT_CHANNEL = "https://t.me/radhesupport" # Update Link
-SUPPORT_CHAT = "https://t.me/+PKYLDIEYiTljMzMx"    # Support Link
+OWNER_ID = int(os.getenv("OWNER_ID", "8482447535"))
+LOG_GROUP_ID = int(os.getenv("LOG_GROUP_ID", "-1003867805165"))
+OWNER_LINK = os.getenv("OWNER_LINK", "https://t.me/XenoEmpir")
+SUPPORT_CHANNEL = os.getenv("SUPPORT_CHANNEL", "https://t.me/radhesupport")
+SUPPORT_CHAT = os.getenv("SUPPORT_CHAT", "https://t.me/+PKYLDIEYiTljMzMx")
