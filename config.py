95fc10306a81d5d0786f708062dfad89b20c4a7b
STARK <misssqn@gmail.com>
2026-02-08 15:42:14 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,17 +1,24 @@
 import os
 
-# --- Bot Credentials ---
+# --- 🔑 Bot Credentials ---
 API_ID = int(os.getenv("API_ID", "34135757")) 
 API_HASH = os.getenv("API_HASH", "d3d5548fe0d98eb1fb793c2c37c9e5c8")
 BOT_TOKEN = os.getenv("BOT_TOKEN", "8508791428:AAEVP8eN8H4yE1AVwSs15KpNV3TamY4hXB8")
 
-# --- Database Setup ---
-# Naye cluster link ke saath updated password
+# --- 🗄️ Database Setup ---
 MONGO_URL = os.getenv("MONGO_URL", "mongodb+srv://misssqn:VICTOR01@cluster0.3otqmso.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
 
-# --- Community & Identity ---
+# --- 👤 Community & Identity ---
 OWNER_ID = int(os.getenv("OWNER_ID", "8482447535"))
 LOG_GROUP_ID = int(os.getenv("LOG_GROUP_ID", "-1003867805165"))
+
+# --- 🔗 Links ---
 OWNER_LINK = os.getenv("OWNER_LINK", "https://t.me/XenoEmpir")
-❂ 𝐒𝛖𝛒𝛒𝛔ʀ𝛕 ❂ = os.getenv("SUPPORT_CHANNEL", "https://t.me/radhesupport")
-❂ 𝐔𝛒ᴅ𝛂𝛕𝛆 ❂ = os.getenv("SUPPORT_CHAT", "https://t.me/+PKYLDIEYiTljMzMx")
+SUPPORT_CHANNEL = os.getenv("SUPPORT_CHANNEL", "https://t.me/radhesupport")
+SUPPORT_CHAT = os.getenv("SUPPORT_CHAT", "https://t.me/+PKYLDIEYiTljMzMx")
+
+# --- 🎨 Stylish Button Labels (Sirf labels ke liye use karein) ---
+ADD_ME_TEXT = "♻ 𝐀ᴅᴅ 𝐌𝝴 𝝸𝝶 𝐘𝞂𝞄𝐑 𝐆𝐑𝞂𝞄𝞀 ♻"
+UPDATE_TEXT = "❂ 𝐔𝛒ᴅ𝛂𝛕𝛆 ❂"
+SUPPORT_TEXT = "❂ 𝐒𝛖𝛒𝛒𝛔ʀ𝛕 ❂"
+OWNER_BUTTON_TEXT = "✮ ❍𝛚𝛈𝛆ʀ ✮"
